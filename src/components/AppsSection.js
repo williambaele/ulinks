33790b497901f0cd78c7dea01b850094055ec7ec
William Baele <baelewilliam1999@gmail.com>
2023-08-30 18:10:36 +0200

    Started UI implementations

--- src/components/AppsSection.js
+++ src/components/AppsSection.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const AppsSection = () => {
-    return (
-        <div className="h-screen bg-red-200">
-            <p>hi</p>
-        </div>
-    );
+  return (
+    <div className="h-screen bg-red-200 flex items-center">
+        <div className="h-[calc(100vh-200px)] bg-green-400 w-full"></div>
+    </div>
+  );
 };
 
 export default AppsSection;
