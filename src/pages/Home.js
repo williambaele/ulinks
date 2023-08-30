291615967481643f1c6aeec5ad32a2be82cf0982
William Baele <baelewilliam1999@gmail.com>
2023-08-30 16:31:09 +0200

    Started UI implementations

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const Home = () => {
+    return (
+        <div className="h-screen bg-[#f3f3f1]">
+            <Header/>
+            
+        </div>
+    );
+};
+
+export default Home;
