a2781b7b63ac310b180fdd5c73df6713b7d4032d
William Baele <baelewilliam1999@gmail.com>
2023-08-30 17:08:17 +0200

    Started UI implementations

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import Header from '../components/Header';
+import React from "react";
+import Header from "../components/Header";
+import HeroBanner from "../components/HeroBanner";
 
 const Home = () => {
-    return (
-        <div className="h-screen bg-[#f3f3f1]">
-            <Header/>
-            
-        </div>
-    );
+  return (
+    <div className="h-screen flex flex-col bg-[#f3f3f1] px-4">
+      <Header />
+      <div className="flex-grow bg-red-200 flex items-center justify-center my-10">
+        <HeroBanner/>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
