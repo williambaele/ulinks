42875afac37a31e2f5c91b3f63cf371257f2f457
William Baele <baelewilliam1999@gmail.com>
2023-08-31 12:07:07 +0200

    UI improvements

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -102,6 +102,58 @@ const Faq = () => {
               </div>
             ))}
           </div>
+          <div
+            class="hs-accordion pt-6 pb-3"
+            id="hs-basic-with-title-and-arrow-stretched-heading-four"
+          >
+            <button
+              class="hs-accordion-toggle group pb-3 inline-flex items-center justify-between gap-x-3 w-full md:text-lg font-semibold text-left text-gray-800 transition hover:text-gray-500 dark:text-gray-200 dark:hover:text-gray-400"
+              aria-controls="hs-basic-with-title-and-arrow-stretched-collapse-four"
+            >
+              How secure is Preline?
+              <svg
+                class="hs-accordion-active:hidden block w-3 h-3 text-gray-600 group-hover:text-gray-500 dark:text-gray-400"
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M2 5L8.16086 10.6869C8.35239 10.8637 8.64761 10.8637 8.83914 10.6869L15 5"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                />
+              </svg>
+              <svg
+                class="hs-accordion-active:block hidden w-3 h-3 text-gray-600 group-hover:text-gray-500 dark:text-gray-400"
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M2 11L8.16086 5.31305C8.35239 5.13625 8.64761 5.13625 8.83914 5.31305L15 11"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                />
+              </svg>
+            </button>
+            <div
+              id="hs-basic-with-title-and-arrow-stretched-collapse-four"
+              class="hs-accordion-content hidden w-full overflow-hidden transition-[height] duration-300"
+              aria-labelledby="hs-basic-with-title-and-arrow-stretched-heading-four"
+            >
+              <p class="text-gray-600 dark:text-gray-400">
+                Protecting the data you trust to Preline is our first priority.
+                This part is really crucial in keeping the project in line to
+                completion.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
