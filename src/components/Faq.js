f9e2292bc38af174ce4448976b51c8db5ad2f750
William Baele <baelewilliam1999@gmail.com>
2023-08-31 12:20:47 +0200

    UI improvements

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -3,36 +3,37 @@ import React, { useState } from "react";
 const Faq = () => {
   const questions = [
     {
-      title: "Lorem",
+      title: "Lorem ipsum linko canolopa",
+      content:
+        "Yes, you can cancel anytime no questions are asked while you cancel but we would highly appreciate if you will give us some feedback anytime no questions are asked while you cancel but we would highly.",
+    },
+    {
+      title: "Lorem ipsum linko canolopa",
       content:
         "Yes, you can cancel anytime no questions are asked while you cancel but we would highly appreciate if you will give us some feedback.",
     },
     {
-      title: "Lorem",
+      title: "Lorem ipsum linko canolopa",
       content:
         "Yes, you can cancel anytime no questions are asked while you cancel but we would highly appreciate if you will give us some feedback.",
     },
     {
-      title: "Lorem",
+      title: "Lorem ipsum linko canolopa",
       content:
         "Yes, you can cancel anytime no questions are asked while you cancel but we would highly appreciate if you will give us some feedback.",
     },
     {
-      title: "Lorem",
-      content:
-        "Yes, you can cancel anytime no questions are asked while you cancel but we would highly appreciate if you will give us some feedback.",
-    },
-    {
-      title: "Lorem",
+      title: "Lorem ipsum linko canolopa",
       content:
         "Yes, you can cancel anytime no questions are asked while you cancel but we would highly appreciate if you will give us some feedback.",
     },
   ];
 
-  const [activeQuestion, setActiveQuestion] = useState("");
+  const [activeQuestion, setActiveQuestion] = useState(0);
+
   return (
-    <div class="w-full px-4 mx-auto h-screen bg-purple-200 flex items-center">
-      <div class="grid md:grid-cols-5 gap-10 bg-green-200">
+    <div class="w-full px-4 mx-auto h-screen flex items-center">
+      <div class="grid md:grid-cols-5 gap-10">
         <div class="md:col-span-2">
           <div class="max-w-xs">
             <h2 class="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">
@@ -47,53 +48,56 @@ const Faq = () => {
         </div>
 
         <div class="md:col-span-3">
-          <div class="hs-accordion-group divide-y divide-gray-200 dark:divide-gray-700">
+          <div class="divide-y divide-gray-200">
             {questions.map((question, index) => (
               <div
+                onClick={() => setActiveQuestion(index)}
                 key={index}
-                class="hs-accordion pb-3 active"
-                id="hs-basic-with-title-and-arrow-stretched-heading-one"
+                className={`"hs-accordion pb-3 active p-2 rounded-xl ${activeQuestion === index ? "bg-white" : ""}`}
               >
                 <button
                   class="hs-accordion-toggle group pb-3 inline-flex items-center justify-between gap-x-3 w-full md:text-lg font-semibold text-left text-gray-800 transition hover:text-gray-500"
                   aria-controls="hs-basic-with-title-and-arrow-stretched-collapse-one"
                 >
                   {question.title}
-                  <svg
-                    class="hs-accordion-active:hidden block w-3 h-3 text-gray-600 group-hover:text-gray-500"
-                    width="16"
-                    height="16"
-                    viewBox="0 0 16 16"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M2 5L8.16086 10.6869C8.35239 10.8637 8.64761 10.8637 8.83914 10.6869L15 5"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                    />
-                  </svg>
-                  <svg
-                    class="hs-accordion-active:block hidden w-3 h-3 text-gray-600 group-hover:text-gray-500"
-                    width="16"
-                    height="16"
-                    viewBox="0 0 16 16"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M2 11L8.16086 5.31305C8.35239 5.13625 8.64761 5.13625 8.83914 5.31305L15 11"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                    />
-                  </svg>
+                  {activeQuestion === index ? (
+                    <svg
+                      class="hs-accordion-active:block  w-3 h-3 text-gray-600 group-hover:text-gray-500"
+                      width="16"
+                      height="16"
+                      viewBox="0 0 16 16"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M2 11L8.16086 5.31305C8.35239 5.13625 8.64761 5.13625 8.83914 5.31305L15 11"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      class="hs-accordion-active:hidden block w-3 h-3 text-gray-600 group-hover:text-gray-500"
+                      width="16"
+                      height="16"
+                      viewBox="0 0 16 16"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M2 5L8.16086 10.6869C8.35239 10.8637 8.64761 10.8637 8.83914 10.6869L15 5"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                      />
+                    </svg>
+                  )}
                 </button>
                 <div
-                  id="hs-basic-with-title-and-arrow-stretched-collapse-one"
-                  class="hs-accordion-content w-full overflow-hidden transition-[height] duration-300"
-                  aria-labelledby="hs-basic-with-title-and-arrow-stretched-heading-one"
+                  className={`hs-accordion-content w-full  overflow-hidden transition-[height] duration-300 ${
+                    activeQuestion === index ? "" : "hidden"
+                  }`}
                 >
                   <p class="text-gray-600 dark:text-gray-400">
                     {question.content}
@@ -101,58 +105,6 @@ const Faq = () => {
                 </div>
               </div>
             ))}
-          </div>
-          <div
-            class="hs-accordion pt-6 pb-3"
-            id="hs-basic-with-title-and-arrow-stretched-heading-four"
-          >
-            <button
-              class="hs-accordion-toggle group pb-3 inline-flex items-center justify-between gap-x-3 w-full md:text-lg font-semibold text-left text-gray-800 transition hover:text-gray-500 dark:text-gray-200 dark:hover:text-gray-400"
-              aria-controls="hs-basic-with-title-and-arrow-stretched-collapse-four"
-            >
-              How secure is Preline?
-              <svg
-                class="hs-accordion-active:hidden block w-3 h-3 text-gray-600 group-hover:text-gray-500 dark:text-gray-400"
-                width="16"
-                height="16"
-                viewBox="0 0 16 16"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M2 5L8.16086 10.6869C8.35239 10.8637 8.64761 10.8637 8.83914 10.6869L15 5"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                />
-              </svg>
-              <svg
-                class="hs-accordion-active:block hidden w-3 h-3 text-gray-600 group-hover:text-gray-500 dark:text-gray-400"
-                width="16"
-                height="16"
-                viewBox="0 0 16 16"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M2 11L8.16086 5.31305C8.35239 5.13625 8.64761 5.13625 8.83914 5.31305L15 11"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                />
-              </svg>
-            </button>
-            <div
-              id="hs-basic-with-title-and-arrow-stretched-collapse-four"
-              class="hs-accordion-content hidden w-full overflow-hidden transition-[height] duration-300"
-              aria-labelledby="hs-basic-with-title-and-arrow-stretched-heading-four"
-            >
-              <p class="text-gray-600 dark:text-gray-400">
-                Protecting the data you trust to Preline is our first priority.
-                This part is really crucial in keeping the project in line to
-                completion.
-              </p>
-            </div>
           </div>
         </div>
       </div>
