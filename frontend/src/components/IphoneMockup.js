b84dc80583cb0cf00880b17318c22d1e4a737fc7
William Baele <baelewilliam1999@gmail.com>
2023-09-06 12:16:45 +0200

    Added dynamic path based on user pseudo

--- frontend/src/components/IphoneMockup.js
+++ frontend/src/components/IphoneMockup.js
@@ -1,7 +1,8 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const IphoneMockup = ({ user, userActiveLinks }) => {
-  console.log(userActiveLinks)
+  console.log(userActiveLinks);
   return (
     <div class="relative mx-auto border-gray-800  bg-gray-800 border-[14px] rounded-[2.5rem] md:h-[600px] h-[350px] md:w-[300px] w-[180px]">
       <div class="h-[32px] w-[3px] bg-gray-800 absolute -left-[17px] top-[72px] rounded-l-lg"></div>
@@ -9,9 +10,11 @@ const IphoneMockup = ({ user, userActiveLinks }) => {
       <div class="h-[46px] w-[3px] bg-gray-800 absolute -left-[17px] top-[178px] rounded-l-lg"></div>
       <div class="h-[64px] w-[3px] bg-gray-800 absolute -right-[17px] top-[142px] rounded-r-lg"></div>
       <div class="rounded-[2rem] overflow-hidden w-[272px] h-[572px] bg-white dark:bg-gray-800"></div>
-      <p className="absolute left-0 right-0 font-bold text-center bottom-10">
-        ulinks.me/{user.link}
-      </p>
+      <Link to={`/${user.pseudo}`}>
+        <p className="absolute left-0 right-0 font-bold text-center bottom-10">
+          ulinks.me/{user.pseudo}
+        </p>
+      </Link>
       <p className="absolute left-0 right-0 font-bold text-center top-32">
         {userActiveLinks.length}
       </p>
