d352dd78f630d096a00bef4bc7033988cdb27f0d
William Baele <baelewilliam1999@gmail.com>
2023-09-08 15:21:18 +0200

    Fixed z index user header

--- frontend/src/components/IphoneMockup.js
+++ frontend/src/components/IphoneMockup.js
@@ -4,12 +4,12 @@ import ClientRow from "./ClientRow";
 
 const IphoneMockup = ({ user, userActiveLinks }) => {
   return (
-    <div class="relative mx-auto border-gray-800 bg-gray-800 border-[14px] rounded-[2.5rem] md:h-[600px] h-[350px] md:w-[300px] w-[180px]">
+    <div class="z-10 relative mx-auto border-gray-800 bg-gray-800 border-[14px] rounded-[2.5rem] md:h-[600px] h-[350px] md:w-[300px] w-[180px]">
       <div class="h-[32px] w-[3px] bg-gray-800 absolute -left-[17px] top-[72px] rounded-l-lg"></div>
       <div class="h-[46px] w-[3px] bg-gray-800 absolute -left-[17px] top-[124px] rounded-l-lg"></div>
       <div class="h-[46px] w-[3px] bg-gray-800 absolute -left-[17px] top-[178px] rounded-l-lg"></div>
       <div class="h-[64px] w-[3px] bg-gray-800 absolute -right-[17px] top-[142px] rounded-r-lg"></div>
-      <div class="rounded-[2rem] overflow-hidden w-[272px] h-[572px] bg-[#f3f3f1]">
+      <div class="z-10 rounded-[2rem] overflow-hidden w-[272px] h-[572px] bg-[#f3f3f1]">
         <div className="flex items-center justify-center h-1/5">
           <p className="text-xl font-bold">
             {user.pseudo}
